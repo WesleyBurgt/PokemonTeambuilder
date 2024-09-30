c506860e8dd4dfeac344497174ab8ef764a6f25e
WesleyBurgt <wesleyburgt@gmail.com>
2024-09-30 10:08:54 +0200

    added allowing localhost 3000 for ui testing

--- PokémonTeambuilder/Program.cs
+++ PokémonTeambuilder/Program.cs
@@ -11,11 +11,21 @@ namespace PokémonTeambuilder
 
             builder.Services.AddControllers();
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowLocalhost3000",
+                    policy => policy.WithOrigins("http://localhost:3000")
+                                    .AllowAnyMethod()
+                                    .AllowAnyHeader());
+            });
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            app.UseCors("AllowLocalhost3000");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
