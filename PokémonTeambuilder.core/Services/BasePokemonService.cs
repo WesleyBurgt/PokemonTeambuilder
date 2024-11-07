b0becdcfb5d1c4796b95122fb52743f5d4b75124
WesleyBurgt <wesleyburgt@gmail.com>
2024-11-07 11:51:18 +0100

    Added null exceptions to ValidateBasePokemon

--- PokémonTeambuilder.core/Services/BasePokemonService.cs
+++ PokémonTeambuilder.core/Services/BasePokemonService.cs
@@ -38,8 +38,12 @@ namespace PokémonTeambuilder.core.Services
                 throw new Exception("Pokemon Id cannot be" + pokemon.Id);
             if (string.IsNullOrEmpty(pokemon.Name))
                 throw new Exception("Pokemon Name cannot be null or empty");
+            if (pokemon.Typings == null)
+                throw new Exception("Pokemon Typing cannot be null");
             if (pokemon.Typings.Count <= 0)
                 throw new Exception("Pokemon cannot have no typing");
+            if (pokemon.BaseStats == null)
+                throw new Exception("Pokemon Stats cannot be null");
             if (pokemon.BaseStats.Hp <= 0)
                 throw new Exception("Pokemon Hp cannot be" + pokemon.BaseStats.Hp);
             if (pokemon.BaseStats.Attack <= 0)
@@ -52,6 +56,8 @@ namespace PokémonTeambuilder.core.Services
                 throw new Exception("Pokemon SpecialDefense cannot be" + pokemon.BaseStats.SpecialDefense);
             if (pokemon.BaseStats.Speed <= 0)
                 throw new Exception("Pokemon Speed cannot be" + pokemon.BaseStats.Speed);
+            if (pokemon.Abilities == null)
+                throw new Exception("Pokemon Abilities cannot be null");
             if (pokemon.Abilities.Count <= 0)
                 throw new Exception("Pokemon cannot have no abilities");
         }
